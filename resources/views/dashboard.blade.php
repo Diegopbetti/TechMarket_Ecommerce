87370c1d230f34f243138db2567880ade48acfc9
Miguelcrf <migueldoliveira1@gmail.com>
2026-01-16 23:27:17 -0300

    responsividade mobile dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ 'Dashboard ' . \Auth::user()->name }}
+        <h2 class="font-semibold text-lg sm:text-xl text-gray-800 leading-tight px-4 sm:px-0">
+            {{ 'Dashboard ' . \Auth::user()->name }}
         </h2>
     </x-slot>
 
-    <div class="py-12 sm:w-full">
+    <div class="py-6 sm:py-12 w-full px-4 sm:px-0">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-4 sm:p-6 text-gray-900 text-sm sm:text-base">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
